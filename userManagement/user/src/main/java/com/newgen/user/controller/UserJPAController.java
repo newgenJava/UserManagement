f315b9f6ac35e426b25fe31a58d5c5205e315bab
newgenJava <ayushnewgen705@gmail.com>
2025-03-21 21:15:37 +0530

    Added Delete and POST APIs

--- userManagement/user/src/main/java/com/newgen/user/controller/UserJPAController.java
+++ userManagement/user/src/main/java/com/newgen/user/controller/UserJPAController.java
@@ -5,37 +5,49 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.newgen.user.model.User;
 import com.newgen.user.services.UserJPAService;
 
 @RestController
 @RequestMapping("/users")
 public class UserJPAController {
-	
+
 	@Autowired
 	private UserJPAService userService;
-	
+
 	@GetMapping
-	public List<User> getUsers(){
+	public List<User> getUsers() {
 		return userService.getAllUsers();
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<User> getUserById(@PathVariable Integer id){
+	public ResponseEntity<User> getUserById(@PathVariable Integer id) {
 		Optional<User> user = userService.getUserByID(id);
-		
-		if(!user.isPresent()) {
-			//Throw errors
+
+		if (!user.isPresent()) {
+			// Throw errors
 		}
-		
-		return user.map(ResponseEntity::ok)
-				.orElseGet(() -> ResponseEntity.notFound().build());
-		
-//		return userService.getAllUsers();
+
+		return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+		userService.deleteUser(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- userManagement/user/src/main/java/com/newgen/user/services/UserJPAService.java
+++ userManagement/user/src/main/java/com/newgen/user/services/UserJPAService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.newgen.user.model.User;
 import com.newgen.user.model.UserRepository;
@@ -23,7 +24,14 @@ public class UserJPAService {
 		return userRepository.findById(id);
 	}
 	
+	public User createUser(User user) {
+		return userRepository.save(user);
+
+	}
 	
-	
+	@Transactional
+	public void deleteUser(Integer id) {
+		userRepository.deleteById(id);
+	}
 	
 }
